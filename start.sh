4929fb106a8b60f61a66da69b660a39753f8bc8b
ziroAU <hudson@ziroau.com>
2023-12-29 15:22:17 +1100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Start NGINX in the background
 nginx &
 
-# Start OpenVSCode Server
+# Starts OpenVSCode Server
 exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token "$@"
